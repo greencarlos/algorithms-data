3b835e9e3a90abe31f107a0548e234d6bf958806
Carlos Green <zonxa14@gmail.com>
2019-05-17 12:45:10 -0700

    Update JackMay16th.js

--- trees/JackMay16th.js
+++ trees/JackMay16th.js
@@ -204,3 +204,46 @@ function minHeightBst(array) {
 
 }
 
+
+
+
+
+
+
+
+
+
+/* 4) Given a sorted binary tree, return the vertical order
+ * traversal of its nodes' values. (ie, column by column
+ * from left to right, and within a column from top to bottom).
+ * If two nodes are in the same row and column (in example below: 9 and 12),
+ * the order should be from left to right (in example below: 9, then 12).
+
+Example:
+a)   input:
+          10
+        /    \
+       5     15
+      / \   /  \
+     3   9 12  17
+          /
+         /
+        11
+
+output: [3, 5, 11, 10, 9, 12, 15, 17]
+
+b) input:
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+    /\
+   /  \
+   5   2
+output: [4,9,5,3,0,1,8,2,7] */
+
+
+
