fb8659468f3153592991c1533983e81913d62475
Carlos Green <zonxa14@gmail.com>
2019-05-17 09:39:02 -0700

    Update JackMay16th.js

--- trees/JackMay16th.js
+++ trees/JackMay16th.js
@@ -104,3 +104,103 @@ const BFS = (node, queue = [], current = null) => {
 }
 
 console.log(BFS(a), "BFS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 2)Implement an iterator over a binary search tree (BST).
+Your iterator will be initialized with the root node of a BST.
+Calling next() will return the next smallest number in the BST.
+*/
+
+const node1 = {val: 7}
+const node2 = {val: 3}
+const node3 = {val: 16}
+const node4 = {val: 9}
+const node5 = {val: 20}
+
+node1.left = node2;
+node1.right = node3;
+
+node3.left = node4;
+node3.right = node5;
+
+/*
+  (7)
+  /  \
+(3)  (16)
+     /  \
+   (9)  (20)
+*/
+
+function BSTIterator(node) {
+} 
+
+
+const next = (node) => {
+  if (!node) return
+  next(node.left);
+  console.log(node.val)
+}
+
+console.log(next(node1))
+
+/*
+BSTIterator.prototype.next = function () {
+  if (!node) return;
+  BSTIterator(node.left)
+}*/
+
+BSTIterator.prototype.hasNext = function () {} 
+
+let iterator = new BSTIterator(root);
+
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false
+
+
+
+
+
+
+
+
+
+
+
+
+/*3)  Given a sorted array, write a program that build
+a minimum height (balanced) binary search tree.
+Note that the tree node constructor class.
+Return the binary search tree root node. */
+
+
+function TreeNode(value) {
+  this.data = value;
+  this.left = null;
+  this.right = null;
+}
+
+function minHeightBst(array) {
+  // your code here
+
+}
+
