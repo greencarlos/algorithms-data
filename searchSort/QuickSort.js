cf3c7644ec2888af9a6c3c860c8d08aad6cb2dcd
Carlos Green <zonxa14@gmail.com>
2019-11-08 15:02:51 -0800

    quicksort

---
+++ searchSort/QuickSort.js
@@ -0,0 +1,49 @@
+// https://www.guru99.com/quicksort-in-javascript.html
+
+var items = [5, 3, 7, 6, 2, 9]
+
+console.log(items, 'items')
+
+function swap(items, leftIndex, rightIndex) {
+  var temp = items[leftIndex]
+  items[leftIndex] = items[rightIndex]
+  items[rightIndex] = temp
+}
+
+function partition(items, left, right) {
+  var pivot = items[Math.floor((right + left) / 2)]
+  var i = left
+  var j = right
+
+  while(i <= j) {
+    while(items[i] < pivot) {
+      i++
+    }
+    while(items[j] > pivot) {
+      j--
+    }
+    if (i <= j) {
+      swap(items, i, j)
+      i++
+      j--
+    }
+  }
+  return i
+}
+
+function quickSort(items, left, right) {
+  var index
+  if (items.length > 1) {
+    index = partition(items, left, right)
+    if (left < index - 1) {
+      quickSort(items, left, index - 1)
+    }
+    if (index < right) {
+      quickSort(items, index, right)
+    }
+  }
+  return items
+}
+
+var sortedArr = quickSort(items, 0, items.length - 1)
+console.log(sortedArr)
