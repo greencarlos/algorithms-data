94061c0fd01dbef4f493ba1990b91f8be1cb940a
Carlos Green <zonxa14@gmail.com>
2019-12-23 16:51:27 -0800

    binary search

--- searchSort/binarySearch.js
+++ searchSort/binarySearch.js
@@ -1,4 +1,5 @@
 const array = [1,4,4,7,7,8,11,19,21,23,24,30]
+const arr = [-1, 0, 3, 5, 9, 12]
 
 function binarySearch(arr, target) {
   // starting values
@@ -35,9 +36,34 @@ function binarySearch(arr, target) {
   return false; // worst case run-time
 }
 
+/**
+ *  * @param number[]} nums
+ *   * @param number} target
+ *    * @return number}
+ *     */
+var search = function(nums, target) {
+  let left = 0
+  let right = nums.length-1;
+
+  while(left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+
+  return -1 
+};
+
 // How binary search works? Say given the sorted
 // array below - [1,4,4,7,7,8,11,19,21,23,24,30],
 // and the target key, 19.
 
-console.log(binarySearch(array, 19))
-// 5 
+console.log(binarySearch(array, 19), 5)
+console.log(search(arr, 9), 4)
+console.log(search(arr, 2), -1)
+
