64ee10352a0677f44a83cb8ad8a11e0fd1c7b6dd
Carlos Green <zonxa14@gmail.com>
2020-01-08 14:14:04 -0800

    leetcode

--- leetcode/easy/100.js
+++ leetcode/easy/100.js
@@ -24,42 +24,10 @@ one.right = three
 
 console.log(a, '', one)
 
-
 const nice = {val: 10}
 const tree = {val: 20}
 
 nice.left = tree
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {boolean}
- */
-/*
-var isSameTree = function(p, q) {
-
-    const dfs = (node1, node2) => {
-      if (!node1 && !node2)  {return true}
-      if (!node1 || !node2) {return false}
-      if (node1.val !== node2.val) {return false}
-      console.log(node1.val, 'node1')
-      console.log(node2.val, 'node2')
-      dfs(node1.left, node2.left)
-      dfs(node1.right, node2.right)
-    }
-
-  dfs(p, q)
-  return true
-};
-*/
-
 
 var isSameTree = function(p, q) {
   if (!p && !q) {
