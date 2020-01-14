05a309ba1adcfc7d29a2d5fbf46322e2540cc595
Carlos Green <zonxa14@gmail.com>
2020-01-14 13:19:14 -0800

    hashing

--- Hashing/Hash.js
+++ Hashing/Hash.js
@@ -17,7 +17,7 @@ const hash = {}
 const hashMap = new Map()
 // proper hashmap in javascript
 
-const arr = [1, 2, 3, 4, 5, 5, 5, 5]
+const arr = [1, 1, 1, 2, 2, 3]
 
 for (let i = 0; i < arr.length; i++) {
   // Object
@@ -31,6 +31,42 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 
+// Array.prototype.reduce
+const reduceHash = (arr, obj={}) => {
+  arr.reduce((acc, curr) => {
+    if (obj[curr] === undefined) {
+      obj[curr] = 1
+    } else {
+      obj[curr] += 1
+    }
+  }, obj)
+
+  return obj
+}
+
+// Recursion object
+const recurse = (arr, i=0, obj={}) => {
+  if (i === arr.length) return obj
+  obj[arr[i]] = (obj[arr[i]] || 0) + 1
+  return recurse(arr, i+1, obj)
+}
+
+// Recusrion Map
+const hashTable = (arr, i=0, hash=new Map()) => {
+  if (i === arr.length) return hash
+  if (hash.has(arr[i])) {
+    hash.set(arr[i], hash.get(arr[i]) + 1)
+  } else {
+    hash.set(arr[i], 1)
+  }
+  return hashTable(arr, i+1, hash)
+}
+
 console.log(arr, 'original')
 console.log(hash, 'hash')
+console.log(" ")
 console.log(hashMap, 'hashMap')
+console.log(reduceHash(arr), 'reduce Hash')
+console.log(" ")
+console.log(recurse(arr), 'recursion hash')
+console.log(hashTable(arr), 'map recurse')
