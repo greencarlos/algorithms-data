3a236dfa76bb14b53ef0ebd19fee7b55eb02624d
Carlos Green <zonxa14@gmail.com>
2020-03-18 18:02:22 -0700

    algos

--- dynamicRecursion/fibonacci.js
+++ dynamicRecursion/fibonacci.js
@@ -4,6 +4,14 @@ const fib = (n) => {
   return fib(n - 1) + fib(n - 2)
 }
 
+function fibon(n) {
+  if (n < 2) {
+    return n
+  }
+
+  return fibon(n-1) + fibon(n-2)
+}
+
 // O(n)
 const fibo = (n, f = []) => {
   f[0] = 0
@@ -15,6 +23,18 @@ const fibo = (n, f = []) => {
   return f[n]
 }
 
+function finona(n, cache) {
+  if (cache[n] !== undefined) {
+    return cache[n]
+  }
+
+  if (n < 2) {
+    cache[n] = n
+  } else {
+    cache[n] fibona(n - 1, cache) + fibona(n - 2, cache)
+  }
+}
+
 console.log(fib(1), 1)
 console.log(fib(5), 5)
 console.log(fib(10), 10)
@@ -22,3 +42,7 @@ console.log(" ")
 console.log(fibo(1), 1)
 console.log(fibo(5), 5)
 console.log(fibo(10), 10)
+console.log(" ")
+console.log(fibon(1), 1)
+console.log(fibon(5), 5)
+console.log(fibon(10), 10)
