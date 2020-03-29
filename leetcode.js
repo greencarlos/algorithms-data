4f0f6ba94be7960e2a7fa08737f66cd9e33f70e0
Carlos Green <zonxa14@gmail.com>
2020-03-28 20:07:36 -0700

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -1,40 +1,33 @@
-function TreeNode(val) {
-  this.val = val;
-  this.left = null 
-  this.right = null;
-}
+var numTeams = function(rating) {
+  const result = []
 
-var largestValues = function(root) {
-  return -1
+  for (let i = 0; i < rating.length; i++) {
+    for (let j = i + 1; j < rating.length; j++) {
+      for (k = j + 1; k < rating.length; k++) {
+        if (rating[i] < rating[j] < rating[k] || 
+          rating[i] > rating[j] > rating[k]) {
+          result.push([rating[i], rating[j], rating[k]])
+        }
+      }
+    }
+  }
+
+  console.log(result, 'result')
+  return result.length 
 };
 
-const one = new TreeNode(1)
-const three = new TreeNode(3)
-const two = new TreeNode(2)
-const five = new TreeNode(5)
-const three2 = new TreeNode(3)
-const nine = new TreeNode(9)
-
-one.left = three
-one.right = two
-
-three.left = five
-three.right = three2
-
-two.right = nine
-
 /*
-Input: 
+Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
 
-          1
-         / \
-        3   2
-       / \   \  
-      5   3   9 
-
-Output: [1, 3, 9]
+A team is valid if:  
+(rating[i] < rating[j] < rating[k]) or 
+(rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
 */
 
-console.log(one, 'root')
-console.log(" ")
-console.log(largestValues(one), [1, 3, 9])
+const rating1 = [2,5,3,4,1]
+const rating2 = [2,1,3]
+const rating3 = [1,2,3,4]
+
+console.log(numTeams(rating1), 3)
+console.log(numTeams(rating2), 0)
+console.log(numTeams(rating3), 4)
