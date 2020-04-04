7dd564d2575d9e96a70366e25ee91d07df8309f1
Carlos Green <zonxa14@gmail.com>
2020-04-04 11:24:19 -0700

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -1,31 +1,68 @@
-// Copy the array with sorted elements
-// hash the elements
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
 
-// if the 
+/*
 
-// O (n log n + n) Time
-// O(n) Space
-var groupAnagrams = function(strs) {
-  const hash = {}
-  for (let i = 0; i < strs.length; i++) {
-    const sorted = strs[i].split('').sort().join('')
-    if (hash[sorted]) {
-      hash[sorted].push(strs[i])
-    } else {
-      hash[sorted] = [strs[i]]
-    }
-  }
-  return Object.values(hash)
+    (3)
+    / \
+  (9) (20)
+      /  \
+    (15) (7)
+*/
+
+const three = new TreeNode(3)
+const nine = new TreeNode(9)
+const twenty = new TreeNode(20)
+const fifteen = new TreeNode(15)
+const seven = new TreeNode(7)
+
+three.left = nine
+three.right = twenty
+
+twenty.left = fifteen
+twenty.right = seven
+
+console.log(three, 'three')
+
+/*
+    (1)
+    / \
+  (2)  (3)
+ /  \  /  \
+(4)(5)(6) (7)
+*/
+
+const one2 = new TreeNode(1)
+const two2 = new TreeNode(2)
+const three2 = new TreeNode(3)
+const four2 = new TreeNode(4)
+const five2 = new TreeNode(5)
+const six2 = new TreeNode(6)
+const seven2 = new TreeNode(7)
+
+one2.left = two2
+one2.right = three2
+
+two2.left = four2
+two2.right = five2
+
+three2.left = six2
+three2.right = seven2
+
+console.log(one2, 'one')
+
+/**
+BFS
+
+
+**/
+var verticalTraversal = function(root) {
+  return -1
 };
 
-const input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+console.log(verticalTraversal(three))
+console.log(verticalTraversal(one2))
 
-const output = [
-  ["ate","eat","tea"],
-  ["nat","tan"],
-  ["bat"]
-]
-
-console.log(groupAnagrams(input))
-console.log(' ')
-console.log(output)
